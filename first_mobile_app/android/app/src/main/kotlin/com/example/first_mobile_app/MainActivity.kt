64960872bdf621073d6b11a7412c6ce37a19fd6c
zishahed <zishahed25@gmail.com>
2026-08-10 19:25:14 +0600

    initial commit

---
+++ first_mobile_app/android/app/src/main/kotlin/com/example/first_mobile_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.first_mobile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ first_mobile_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: Center(child: Text("Hello World"))),
+    );
+  }
+}
+
+
+
+// Types of Widget
+// 1. Stateless
+// 2. Stateful
+// 3. Inherited
+
+// What is State? refers data that determines how widgets look like
+
+// There are major two design rules
+// Material and Cupertino
