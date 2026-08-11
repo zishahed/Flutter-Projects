8a349c325630b50c0bcbe8d23ec86da4d7d64c03
zishahed <zishahed25@gmail.com>
2026-08-11 23:07:42 +0600

    ready template

--- currency_converter_app/lib/home_page.dart
+++ currency_converter_app/lib/home_page.dart
@@ -4,6 +4,29 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Currency Converter",
+              style: TextStyle(
+                color: Colors.indigo,
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold, // w700 = bold, w400 = normal
+              ),
+            ),
+            TextField(
+              style: TextStyle(color: Colors.indigo),
+              decoration: InputDecoration(
+                labelText: "Please enter the amount in USD",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
