47f9f9cf7e7a5c5fbce3d6c3549e7c3ce7a44a1f
zishahed <zishahed25@gmail.com>
2026-08-12 18:39:53 +0600

    add first input sized box

--- currency_converter_app/lib/home_page.dart
+++ currency_converter_app/lib/home_page.dart
@@ -5,6 +5,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> currency = ["USD", "BDT", "JPY", "INR", "EUR"];
+
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 245, 247, 250),
       body: Center(
@@ -19,11 +21,76 @@ class HomePage extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
+            const SizedBox(height: 40),
+
+            SizedBox(
+              width: 450,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.monetization_on_outlined),
+                        prefixIconColor: Color.fromARGB(255, 107, 114, 128),
+
+                        hintText: "Please enter Amount",
+                        hintStyle: const TextStyle(
+                          color: Color.fromARGB(255, 107, 114, 128),
+                        ),
+
+                        filled: true,
+                        fillColor: Colors.white,
+
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide.none,
+                        ),
+
+                        contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+
+                  SizedBox(
+                    width: 120,
+                    child: DropdownButtonFormField<String>(
+                      initialValue: currency[0],
+
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide.none,
+                        ),
+
+                        contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 18,
+                        ),
+                      ),
+
+                      items: currency.map((item) {
+                        return DropdownMenuItem<String>(
+                          value: item,
+                          child: Text(item),
+                        );
+                      }).toList(),
+
+                      onChanged: (initialValue) {
+                        debugPrint("Selected currency: $initialValue");
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
-          // Using Sized Box
-          // TextField     CurrencyUnit
-          // OutputField   CurrencyUnit
-          // Logic triggers on every digit typed
         ),
       ),
     );
