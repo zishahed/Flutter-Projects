191e222540fb5ef29123d7230b08f9da9fd4ba08
zishahed <zishahed25@gmail.com>
2026-08-12 20:02:11 +0600

    ready final skelaton for currency converter

--- currency_converter_app/lib/home_page.dart
+++ currency_converter_app/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,6 +7,13 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<String> currency = ["USD", "BDT", "JPY", "INR", "EUR"];
+
+    final border = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: BorderSide.none,
+    );
+
+    final padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 18);
 
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 245, 247, 250),
@@ -29,6 +37,21 @@ class HomePage extends StatelessWidget {
                 children: [
                   Expanded(
                     child: TextField(
+                      keyboardType: const TextInputType.numberWithOptions(
+                        signed: false, // denying - sign, negative numbers
+                        decimal: true, // allowing dot
+                      ),
+
+                      // this controls what Flutter accepts
+                      // by typing this Formatter I am denying the use of
+                      // negative (-) sign even though it is in the keyboard
+                      // this is imported from services.dart package
+                      inputFormatters: [
+                        FilteringTextInputFormatter.allow(
+                          RegExp(r'^\d*\.?\d*'),
+                        ),
+                      ],
+
                       decoration: InputDecoration(
                         prefixIcon: Icon(Icons.monetization_on_outlined),
                         prefixIconColor: Color.fromARGB(255, 107, 114, 128),
@@ -41,15 +64,10 @@ class HomePage extends StatelessWidget {
                         filled: true,
                         fillColor: Colors.white,
 
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: BorderSide.none,
-                        ),
+                        focusedBorder: border,
+                        enabledBorder: border,
 
-                        contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 16,
-                          vertical: 18,
-                        ),
+                        contentPadding: padding,
                       ),
                     ),
                   ),
@@ -64,15 +82,70 @@ class HomePage extends StatelessWidget {
                         filled: true,
                         fillColor: Colors.white,
 
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: BorderSide.none,
-                        ),
+                        focusedBorder: border,
+                        enabledBorder: border,
 
-                        contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 16,
-                          vertical: 18,
+                        contentPadding: padding,
+                      ),
+
+                      items: currency.map((item) {
+                        return DropdownMenuItem<String>(
+                          value: item,
+                          child: Text(item),
+                        );
+                      }).toList(),
+
+                      onChanged: (initialValue) {
+                        debugPrint("Selected currency: $initialValue");
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(padding: EdgeInsets.all(12)),
+
+            SizedBox(
+              width: 450,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.monetization_on_outlined),
+                        prefixIconColor: Color.fromARGB(255, 107, 114, 128),
+
+                        hintText: "Converted Amount",
+                        hintStyle: const TextStyle(
+                          color: Color.fromARGB(255, 107, 114, 128),
                         ),
+                        filled: true,
+                        fillColor: Colors.white,
+
+                        focusedBorder: border,
+                        enabledBorder: border,
+
+                        contentPadding: padding,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+
+                  SizedBox(
+                    width: 120,
+                    child: DropdownButtonFormField<String>(
+                      initialValue: currency[0],
+
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+
+                        focusedBorder: border,
+                        enabledBorder: border,
+
+                        contentPadding: padding,
                       ),
 
                       items: currency.map((item) {
