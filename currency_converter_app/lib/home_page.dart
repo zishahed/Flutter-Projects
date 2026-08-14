0b408aeb4d2fa9038d1187657c30fb9450dd45d4
zishahed <zishahed25@gmail.com>
2026-08-14 19:45:47 +0600

    make it Stateful

--- currency_converter_app/lib/home_page.dart
+++ currency_converter_app/lib/home_page.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final List<String> currency = ["USD", "BDT", "JPY", "INR", "EUR"];
 
     final border = OutlineInputBorder(
       borderRadius: BorderRadius.circular(12),
-      borderSide: BorderSide.none,
+      borderSide: BorderSide(
+        color: Color.fromARGB(255, 31, 41, 55),
+        width: 2.0,
+      ),
     );
 
     final padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 18);
@@ -33,134 +41,140 @@ class HomePage extends StatelessWidget {
 
             SizedBox(
               width: 450,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      keyboardType: const TextInputType.numberWithOptions(
-                        signed: false, // denying - sign, negative numbers
-                        decimal: true, // allowing dot
-                      ),
-
-                      // this controls what Flutter accepts
-                      // by typing this Formatter I am denying the use of
-                      // negative (-) sign even though it is in the keyboard
-                      // this is imported from services.dart package
-                      inputFormatters: [
-                        FilteringTextInputFormatter.allow(
-                          RegExp(r'^\d*\.?\d*'),
-                        ),
-                      ],
-
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.monetization_on_outlined),
-                        prefixIconColor: Color.fromARGB(255, 107, 114, 128),
-
-                        hintText: "Please enter Amount",
-                        hintStyle: const TextStyle(
-                          color: Color.fromARGB(255, 107, 114, 128),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        keyboardType: const TextInputType.numberWithOptions(
+                          signed: false, // denying - sign, negative numbers
+                          decimal: true, // allowing dot
                         ),
 
-                        filled: true,
-                        fillColor: Colors.white,
+                        // this controls what Flutter accepts
+                        // by typing this Formatter I am denying the use of
+                        // negative (-) sign even though it is in the keyboard
+                        // this is imported from services.dart package
+                        inputFormatters: [
+                          FilteringTextInputFormatter.allow(
+                            RegExp(r'^\d*\.?\d*'),
+                          ),
+                        ],
 
-                        focusedBorder: border,
-                        enabledBorder: border,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.monetization_on_outlined),
+                          prefixIconColor: Color.fromARGB(255, 107, 114, 128),
 
-                        contentPadding: padding,
+                          hintText: "Please enter Amount",
+                          hintStyle: const TextStyle(
+                            color: Color.fromARGB(255, 107, 114, 128),
+                          ),
+
+                          filled: true,
+                          fillColor: Colors.white,
+
+                          focusedBorder: border,
+                          enabledBorder: border,
+
+                          contentPadding: padding,
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 12),
+                    const SizedBox(width: 12),
 
-                  SizedBox(
-                    width: 120,
-                    child: DropdownButtonFormField<String>(
-                      initialValue: currency[0],
+                    SizedBox(
+                      width: 120,
+                      child: DropdownButtonFormField<String>(
+                        initialValue: currency[0],
 
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
 
-                        focusedBorder: border,
-                        enabledBorder: border,
+                          focusedBorder: border,
+                          enabledBorder: border,
 
-                        contentPadding: padding,
+                          contentPadding: padding,
+                        ),
+
+                        items: currency.map((item) {
+                          return DropdownMenuItem<String>(
+                            value: item,
+                            child: Text(item),
+                          );
+                        }).toList(),
+
+                        onChanged: (initialValue) {
+                          // Take Input from here
+                        },
                       ),
-
-                      items: currency.map((item) {
-                        return DropdownMenuItem<String>(
-                          value: item,
-                          child: Text(item),
-                        );
-                      }).toList(),
-
-                      onChanged: (initialValue) {
-                        debugPrint("Selected currency: $initialValue");
-                      },
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
 
-            Padding(padding: EdgeInsets.all(12)),
+            Padding(padding: EdgeInsets.only(top: 12)),
 
             SizedBox(
               width: 450,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      readOnly: true,
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.monetization_on_outlined),
-                        prefixIconColor: Color.fromARGB(255, 107, 114, 128),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        readOnly: true,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.monetization_on_outlined),
+                          prefixIconColor: Color.fromARGB(255, 107, 114, 128),
 
-                        hintText: "Converted Amount",
-                        hintStyle: const TextStyle(
-                          color: Color.fromARGB(255, 107, 114, 128),
+                          hintText: "Converted Amount",
+                          hintStyle: const TextStyle(
+                            color: Color.fromARGB(255, 107, 114, 128),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+
+                          focusedBorder: border,
+                          enabledBorder: border,
+
+                          contentPadding: padding,
                         ),
-                        filled: true,
-                        fillColor: Colors.white,
-
-                        focusedBorder: border,
-                        enabledBorder: border,
-
-                        contentPadding: padding,
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 12),
+                    const SizedBox(width: 12),
 
-                  SizedBox(
-                    width: 120,
-                    child: DropdownButtonFormField<String>(
-                      initialValue: currency[0],
+                    SizedBox(
+                      width: 120,
+                      child: DropdownButtonFormField<String>(
+                        initialValue: currency[0],
 
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
 
-                        focusedBorder: border,
-                        enabledBorder: border,
+                          focusedBorder: border,
+                          enabledBorder: border,
 
-                        contentPadding: padding,
+                          contentPadding: padding,
+                        ),
+
+                        items: currency.map((item) {
+                          return DropdownMenuItem<String>(
+                            value: item,
+                            child: Text(item),
+                          );
+                        }).toList(),
+
+                        onChanged: (initialValue) {
+                          // Show output here
+                        },
                       ),
-
-                      items: currency.map((item) {
-                        return DropdownMenuItem<String>(
-                          value: item,
-                          child: Text(item),
-                        );
-                      }).toList(),
-
-                      onChanged: (initialValue) {
-                        debugPrint("Selected currency: $initialValue");
-                      },
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
