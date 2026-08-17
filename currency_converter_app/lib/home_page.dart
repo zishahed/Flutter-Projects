6ca743336cea0496a4e13993b31bbd9cf95ea9a8
zishahed <zishahed25@gmail.com>
2026-08-17 21:19:23 +0600

    add conversion logic and apply

--- currency_converter_app/lib/home_page.dart
+++ currency_converter_app/lib/home_page.dart
@@ -9,20 +9,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> currency = ["USD", "BDT", "JPY", "INR", "EUR"];
+  final Map<String, double> rates = {
+    "USD": 1.0,
+    "BDT": 122.724,
+    "EUR": 0.85,
+    "INR": 86.5,
+    "JPY": 147.0,
+  };
+  final padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 18);
+  final TextEditingController textEditingController = TextEditingController();
+  double convertedAmount = 0;
+
+  // Conversion Logic
+  double convertCurrency(double amount, String from, String to) {
+    final amountRelativeToUSD = amount / rates[from]!;
+    return amountRelativeToUSD * rates[to]!;
+  }
+
+  String fromCurrency = "USD";
+  String toCurrency = "BDT";
+
+  final border = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(12),
+    borderSide: BorderSide(color: Color.fromARGB(255, 31, 41, 55), width: 2.0),
+  );
+
   @override
   Widget build(BuildContext context) {
-    final List<String> currency = ["USD", "BDT", "JPY", "INR", "EUR"];
-
-    final border = OutlineInputBorder(
-      borderRadius: BorderRadius.circular(12),
-      borderSide: BorderSide(
-        color: Color.fromARGB(255, 31, 41, 55),
-        width: 2.0,
-      ),
-    );
-
-    final padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 18);
-
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 245, 247, 250),
       body: Center(
@@ -47,6 +61,25 @@ class _HomePageState extends State<HomePage> {
                   children: [
                     Expanded(
                       child: TextField(
+                        controller: textEditingController,
+
+                        onChanged: (value) {
+                          final double? amount = double.tryParse(value);
+                          if (amount == null) {
+                            setState(() {
+                              convertedAmount = 0;
+                            });
+                            return;
+                          }
+                          setState(() {
+                            convertedAmount = convertCurrency(
+                              amount,
+                              fromCurrency,
+                              toCurrency,
+                            );
+                          });
+                        },
+
                         keyboardType: const TextInputType.numberWithOptions(
                           signed: false, // denying - sign, negative numbers
                           decimal: true, // allowing dot
@@ -86,7 +119,7 @@ class _HomePageState extends State<HomePage> {
                     SizedBox(
                       width: 120,
                       child: DropdownButtonFormField<String>(
-                        initialValue: currency[0],
+                        initialValue: fromCurrency,
 
                         decoration: InputDecoration(
                           filled: true,
@@ -105,8 +138,24 @@ class _HomePageState extends State<HomePage> {
                           );
                         }).toList(),
 
-                        onChanged: (initialValue) {
-                          // Take Input from here
+                        onChanged: (value) {
+                          setState(() {
+                            fromCurrency = value!;
+                          });
+
+                          final double? amount = double.tryParse(
+                            textEditingController.text,
+                          );
+
+                          if (amount != null) {
+                            setState(() {
+                              convertedAmount = convertCurrency(
+                                amount,
+                                fromCurrency,
+                                toCurrency,
+                              );
+                            });
+                          }
                         },
                       ),
                     ),
@@ -118,63 +167,95 @@ class _HomePageState extends State<HomePage> {
             Padding(padding: EdgeInsets.only(top: 12)),
 
             SizedBox(
-              width: 450,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        readOnly: true,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.monetization_on_outlined),
-                          prefixIconColor: Color.fromARGB(255, 107, 114, 128),
+              width: 390,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 60,
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
 
-                          hintText: "Converted Amount",
-                          hintStyle: const TextStyle(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: const Color.fromARGB(255, 31, 41, 55),
+                          width: 2,
+                        ),
+                      ),
+
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.monetization_on_outlined,
                             color: Color.fromARGB(255, 107, 114, 128),
                           ),
-                          filled: true,
-                          fillColor: Colors.white,
 
-                          focusedBorder: border,
-                          enabledBorder: border,
+                          const SizedBox(width: 12),
 
-                          contentPadding: padding,
-                        ),
+                          Text(
+                            convertedAmount == 0
+                                ? "Converted Amount"
+                                : convertedAmount.toStringAsFixed(2),
+
+                            style: TextStyle(
+                              color: convertedAmount == 0
+                                  ? const Color.fromARGB(255, 107, 114, 128)
+                                  : const Color.fromARGB(255, 31, 41, 55),
+
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    const SizedBox(width: 12),
+                  ),
+                  const SizedBox(width: 12),
 
-                    SizedBox(
-                      width: 120,
-                      child: DropdownButtonFormField<String>(
-                        initialValue: currency[0],
+                  SizedBox(
+                    width: 120,
+                    child: DropdownButtonFormField<String>(
+                      initialValue: toCurrency,
 
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
 
-                          focusedBorder: border,
-                          enabledBorder: border,
+                        focusedBorder: border,
+                        enabledBorder: border,
 
-                          contentPadding: padding,
-                        ),
-
-                        items: currency.map((item) {
-                          return DropdownMenuItem<String>(
-                            value: item,
-                            child: Text(item),
-                          );
-                        }).toList(),
-
-                        onChanged: (initialValue) {
-                          // Show output here
-                        },
+                        contentPadding: padding,
                       ),
+
+                      items: currency.map((item) {
+                        return DropdownMenuItem<String>(
+                          value: item,
+                          child: Text(item),
+                        );
+                      }).toList(),
+
+                      onChanged: (value) {
+                        setState(() {
+                          toCurrency = value!;
+                        });
+
+                        final double? amount = double.tryParse(
+                          textEditingController.text,
+                        );
+
+                        if (amount != null) {
+                          setState(() {
+                            convertedAmount = convertCurrency(
+                              amount,
+                              fromCurrency,
+                              toCurrency,
+                            );
+                          });
+                        }
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
--- currency_converter_app/lib/main.dart
+++ currency_converter_app/lib/main.dart
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return const MaterialApp(home: HomePage());
   }
 }
