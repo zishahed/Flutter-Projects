bd42349a29899e7f9335c3150a26a36a93ce4071
anikazaman370-glitch <anikazaman370@gmail.com>
2026-08-22 00:01:46 +0600

    add player label

--- dice_roll_game/lib/main.dart
+++ dice_roll_game/lib/main.dart
@@ -22,7 +22,7 @@ class DiceDemoPage extends StatefulWidget {
 }
 
 class _DiceDemoPageState extends State<DiceDemoPage> {
-  int _currentDiceValue = 1; // To store the dice value
+  int _currentDiceValue = 6; // To store the dice value
   Key _diceKey = UniqueKey(); // To re-trigger the roll animation
 
   void _rollTheDice() {
@@ -36,40 +36,66 @@ class _DiceDemoPageState extends State<DiceDemoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('RollDice2D Example')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RollDice2D(
-              key: _diceKey, // Assign the key here
-              onRoll: (int value) {
-                // This callback is triggered after the rolling animation completes (if any).
-                // It provides the final value of the dice.
-                setState(() {
-                  _currentDiceValue = value;
-                });
-                print('Dice rolled, final value: $value');
-              },
-              color: DiceColor.red, // Or DiceColor.white
-              rollingTimes:
-                  5, // Number of times the dice face changes during animation
-              // If 1 or less, shows initial face and calls onRoll immediately.
-              speed:
-                  150, // Duration in milliseconds for each face change animation
+      appBar: AppBar(title: const Text('RollDice2D Game')),
+      body: Column(
+        children: [
+          // Player labels
+          Padding(
+            padding: const EdgeInsetsGeometry.symmetric(
+              vertical: 24.0,
+              horizontal: 24.0,
             ),
-            const SizedBox(height: 20),
-            Text(
-              'Dice Value: $_currentDiceValue',
-              style: Theme.of(context).textTheme.headlineMedium,
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "Player 1",
+                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Player 2",
+                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _rollTheDice,
-              child: const Text('Roll Again'),
+          ),
+          // Dice animation
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RollDice2D(
+                    key: _diceKey, // Assign the key here
+                    onRoll: (int value) {
+                      // This callback is triggered after the rolling animation completes (if any).
+                      // It provides the final value of the dice.
+                      setState(() {
+                        _currentDiceValue = value;
+                      });
+                    },
+                    color: DiceColor.white, // Or DiceColor.white
+                    rollingTimes: 5,
+                    // Number of times the dice face changes during animation
+                    // If 1 or less, shows initial face and calls onRoll immediately.
+                    speed:
+                        150, // Duration in milliseconds for each face change animation
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Dice Value: $_currentDiceValue',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _rollTheDice,
+                    child: const Text('Roll Again'),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
